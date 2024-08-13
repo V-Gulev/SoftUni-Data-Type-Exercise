02dafa447c01e155f0dce5b45ca481f6a7308289
V-Gulev <velizargulev@abv.bg>
2024-08-13 20:24:33 +0300

    Centuries to Minutes

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        if (a==a.toUpperCase()){
-            System.out.println("upper-case");
-        }else System.out.println("lower-case");
+        int a = sc.nextInt();
+        double years = a*100;
+        double days= years*365.2422;
+        double hours =days*24;
+        double minutes = hours*60;
+        System.out.printf("%d centuries = %.0f years = %.0f days = %.0f hours = %.0f minutes",a,years,days,hours,minutes);
+
     }
 }
