8fff4c8ccb35141cb9842185ec2b9d38046fbc77
V-Gulev <velizargulev@abv.bg>
2024-08-13 20:26:37 +0300

    Special Numbers

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        double years = a*100;
-        double days= years*365.2422;
-        double hours =days*24;
-        double minutes = hours*60;
-        System.out.printf("%d centuries = %.0f years = %.0f days = %.0f hours = %.0f minutes",a,years,days,hours,minutes);
+        int number = Integer.parseInt(sc.nextLine());
+        double sum = 0.0;
+        int iValue;
+        for (int i = 1; i <= number; i++) {
+            iValue=i;
+            if (i >= 10) {
+                while ( i != 0) {
+                    int digit = i % 10;
+                    sum += digit;
+                    i /= 10;
+                }
+            } else sum += i;
+            i= iValue;
+            if (sum == 5 || sum == 7 || sum == 11) {
+                System.out.println(i + " -> " + "True");
+            } else System.out.println(i + " -> " + "False");
+            sum=0;
+        }
 
     }
 }
+
