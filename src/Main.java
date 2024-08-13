5ff865b08745d948ce8477d9dd3c4a5daf14d050
V-Gulev <velizargulev@abv.bg>
2024-08-13 20:22:45 +0300

    Lower or Upper

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
-        String b = sc.nextLine();
-        String c = sc.nextLine();
-        String reversed = c+" "+b+" "+a;
-        System.out.println(reversed);
+        if (a==a.toUpperCase()){
+            System.out.println("upper-case");
+        }else System.out.println("lower-case");
     }
 }
