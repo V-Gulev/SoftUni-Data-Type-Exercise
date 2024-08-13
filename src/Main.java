b52e7594018ce9b05e6a964c84e700b0609e66c3
V-Gulev <velizargulev@abv.bg>
2024-08-13 20:20:02 +0300

    Chars to String

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name1 = sc.nextLine();
-        String name2 = sc.nextLine();
-        String symbol = sc.nextLine();
-        System.out.println(name1+symbol+name2);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String c = sc.nextLine();
+        System.out.println(a+b+c);
     }
 }
