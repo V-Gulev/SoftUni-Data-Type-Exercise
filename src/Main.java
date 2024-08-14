ab524612f2cb0306740bd39cde8d81d275a0a066
V-Gulev <velizargulev@abv.bg>
2024-08-14 19:34:00 +0300

    Spice Must Flow

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n1 = Integer.parseInt(scanner.nextLine());
-        double sum ;
-        double bigestKeg = 0;
-        String kegName = "";
-        for (int i = 0; i < n1; i++) {
-            String model = scanner.nextLine();
-            double radius= Double.parseDouble(scanner.nextLine());
-            int height = Integer.parseInt(scanner.nextLine());
-            sum = Math.PI*radius*radius*height;
-            if (sum>=bigestKeg){
-                bigestKeg=sum;
-                kegName=model;
-            }
+        int starting = Integer.parseInt(scanner.nextLine());
+
+        int total = 0;
+        int days = 0;
+
+        while (starting >= 100) {
+            total += starting;
+            starting -= 10;
+            days++;
+            total -= 26;
         }
-        System.out.printf("%s",kegName);
+
+        if (total >= 26) {
+            total -= 26;
+        }
+
+        System.out.println(days);
+        System.out.println(total);
+
+
     }
 }
 
