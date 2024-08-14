3fd33b292cac74a87043f1c24276f90c489b0562
V-Gulev <velizargulev@abv.bg>
2024-08-14 19:28:10 +0300

    Triples of Latin Letters

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int start = Integer.parseInt(scanner.nextLine());
-        int end = Integer.parseInt(scanner.nextLine());
-        for (int i = start; i <= end; i++) {
-            System.out.print((char) i + " ");
+        int n1 = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n1; i++) {
+            char firstChar = (char) ('a' + i);
+            for (int j = 0; j < n1; j++) {
+                char secondChar = (char) ('a' + j);
+                for (int k = 0; k < n1; k++) {
+                    char thirdChar = (char) ('a' + k);
+                    System.out.printf("%c%c%c%n", firstChar, secondChar, thirdChar);
+                }
+            }
         }
-
 
     }
 }
