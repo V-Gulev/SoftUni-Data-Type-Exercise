0d8214ab6e49e4ade7fc2e518ceecc2cf61c07ae
V-Gulev <velizargulev@abv.bg>
2024-08-14 19:26:01 +0300

    Elevator

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n1 = Integer.parseInt(scanner.nextLine());
-        int sum =0;
-        while (n1!=0){
-            sum += n1%10;
-            n1/=10;
-        }
-        System.out.println(sum);
+        int people = Integer.parseInt(scanner.nextLine());
+        int capacity = Integer.parseInt(scanner.nextLine());
+        int courses = (int) Math.ceil((double) people /capacity);
+        System.out.println(courses);
     }
 }
 
