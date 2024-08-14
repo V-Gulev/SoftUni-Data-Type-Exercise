e12fb4dc219372d08e3ac1dbf7e48e036b53cb8d
V-Gulev <velizargulev@abv.bg>
2024-08-14 19:28:52 +0300

    Water Overflow

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n1 = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
         for (int i = 0; i < n1; i++) {
-            char firstChar = (char) ('a' + i);
-            for (int j = 0; j < n1; j++) {
-                char secondChar = (char) ('a' + j);
-                for (int k = 0; k < n1; k++) {
-                    char thirdChar = (char) ('a' + k);
-                    System.out.printf("%c%c%c%n", firstChar, secondChar, thirdChar);
-                }
+            int quantity = Integer.parseInt(scanner.nextLine());
+            sum += quantity;
+            if (sum > 255) {
+                sum -= quantity;
+                System.out.println("Insufficient capacity!");
             }
         }
-
+        System.out.println(sum);
     }
 }
 
