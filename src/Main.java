801faac781fd4acae24b7c79728ef0d79aa77b6e
V-Gulev <velizargulev@abv.bg>
2024-08-14 19:15:43 +0300

    Integer Operations

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int checkStop = Integer.parseInt(scanner.nextLine());
-        int sum = 0;
-        int valueOfI ;
-        boolean ifSpecial;
-        for (int i = 1; i <= checkStop; i++) {
-            valueOfI = i;
-            while (i > 0) {
-                sum += i % 10;
-                i = i / 10;
-            }
-            ifSpecial = (sum == 5) || (sum == 7) || (sum == 11);
-            if (!ifSpecial){
-                System.out.printf("%d -> False%n", valueOfI);
-            }else System.out.printf("%d -> True%n", valueOfI);
-            sum = 0;
-            i = valueOfI;
-        }
-
+        int n1 = Integer.parseInt(scanner.nextLine());
+        int n2 = Integer.parseInt(scanner.nextLine());
+        int n3 = Integer.parseInt(scanner.nextLine());
+        int n4 = Integer.parseInt(scanner.nextLine());
+        int answer = (n1+n2)/n3*n4;
+        System.out.println(answer);
     }
 }
 
