290e8d2d7db37131549b79bda94cda542edc534b
V-Gulev <velizargulev@abv.bg>
2024-08-14 19:33:22 +0300

    Beer Kegs

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n1 = Integer.parseInt(scanner.nextLine());
-        int sum = 0;
+        double sum ;
+        double bigestKeg = 0;
+        String kegName = "";
         for (int i = 0; i < n1; i++) {
-            int quantity = Integer.parseInt(scanner.nextLine());
-            sum += quantity;
-            if (sum > 255) {
-                sum -= quantity;
-                System.out.println("Insufficient capacity!");
+            String model = scanner.nextLine();
+            double radius= Double.parseDouble(scanner.nextLine());
+            int height = Integer.parseInt(scanner.nextLine());
+            sum = Math.PI*radius*radius*height;
+            if (sum>=bigestKeg){
+                bigestKeg=sum;
+                kegName=model;
             }
         }
-        System.out.println(sum);
+        System.out.printf("%s",kegName);
     }
 }
 
