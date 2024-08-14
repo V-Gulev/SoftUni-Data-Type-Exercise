8f1e3ee1529de546da405f2978f632872ba9d2bf
V-Gulev <velizargulev@abv.bg>
2024-08-14 19:41:46 +0300

    Data Type Finder

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,59 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    public static double[] arrayFromInput(String s) {
+
+        String[] items = s.split(" ");
+        double[] numbers = new double[items.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Double.parseDouble(items[i]);
+        }
+        return numbers;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numberSnowballs = Integer.parseInt(scanner.nextLine());
-        double biggestValue = 0.0;
-        int biggestSnow = 0;
-        int biggestTime = 0;
-        int biggestQuality = 0;
 
-        for (int i = 0; i < numberSnowballs; i++) {
-            int snow = Integer.parseInt(scanner.nextLine());
-            int time = Integer.parseInt(scanner.nextLine());
-            int quality = Integer.parseInt(scanner.nextLine());
-            double value = Math.pow((double) snow / time, quality);
-
-            if (value > biggestValue) {
-                biggestValue = value;
-                biggestSnow = snow;
-                biggestQuality = quality;
-                biggestTime = time;
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("END")) {
+                break;
             }
+            if (Main.isInt(input)) {
+                System.out.printf("%s is integer type%n", input);
+            } else if (Main.isDouble(input)) {
+                System.out.printf("%s is floating point type%n", input);
+            } else if (input.length() < 2) {
+                System.out.printf("%s is character type%n", input);
+            } else if (Main.isBoolean(input)) {
+                System.out.printf("%s is boolean type%n", input);
+            } else System.out.printf("%s is string type%n", input);
         }
-        System.out.printf("%d : %d = %.0f (%d)", biggestSnow, biggestTime, biggestValue, biggestQuality);
-        scanner.close();
     }
+
+    static boolean isBoolean(String s) {
+        return s.equalsIgnoreCase("true") || s.equalsIgnoreCase("false");
+    }
+
+
+    static boolean isDouble(String s) {
+        try {
+            double i = Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException er) {
+            return false;
+        }
+    }
+
+    static boolean isInt(String s) {
+        try {
+            int i = Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException er) {
+            return false;
+        }
+    }
+
 }
+
