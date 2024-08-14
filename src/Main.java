68aca7564558f4d17a9c60bccd59826147f2410b
V-Gulev <velizargulev@abv.bg>
2024-08-14 19:36:11 +0300

    Snowballs

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int power = Integer.parseInt(scanner.nextLine());
-        int distance = Integer.parseInt(scanner.nextLine());
-        int exhaustionFactor = Integer.parseInt(scanner.nextLine());
-        int pokedCounter = 0;
-        int originalValueOfPower = power;
-        while (power >= distance) {
-            power -= distance;
-            pokedCounter++;
-            if (power == (double) originalValueOfPower / 2 && exhaustionFactor != 0) {
-                power/=  exhaustionFactor;
+        int numberSnowballs = Integer.parseInt(scanner.nextLine());
+        double biggestValue = 0.0;
+        int biggestSnow = 0;
+        int biggestTime = 0;
+        int biggestQuality = 0;
+
+        for (int i = 0; i < numberSnowballs; i++) {
+            int snow = Integer.parseInt(scanner.nextLine());
+            int time = Integer.parseInt(scanner.nextLine());
+            int quality = Integer.parseInt(scanner.nextLine());
+            double value = Math.pow((double) snow / time, quality);
+
+            if (value > biggestValue) {
+                biggestValue = value;
+                biggestSnow = snow;
+                biggestQuality = quality;
+                biggestTime = time;
             }
         }
-        System.out.println(power);
-        System.out.println(pokedCounter);
+        System.out.printf("%d : %d = %.0f (%d)", biggestSnow, biggestTime, biggestValue, biggestQuality);
+        scanner.close();
     }
 }
